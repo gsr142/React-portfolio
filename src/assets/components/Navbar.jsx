777f7cd13d6168193ddfe183bd0df7cbf4349fde
gsr142 <gsr142@gmail.com>
2023-08-30 22:08:31 -0700

    add bootstrap navbar. links functioning

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,7 +1,43 @@
 export default function Navbar() {
-    return (
-        <div>
-            <h1>Navbar</h1>
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Greg Richardson
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  About Me
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="portfolio">
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-    );
+      </nav>
+    </div>
+  );
 }
